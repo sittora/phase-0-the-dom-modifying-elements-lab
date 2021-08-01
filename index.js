bfcd934faf0336eaf12ab052670b6f12990ec03d
sittora <sittora@gmail.com>
2021-07-31 23:33:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+console.log("******** hello *********")
+
+let selector = document.querySelector("main");
+selector.remove(); 
+
+const newHeader = document.createElement('h1'); 
+newHeader.idName = "victory";
+newHeader.setAttribute("id", "victory");
+const p = document.createElement('p');
+  p.innerHTML = "Sitora is the champion";
+  newHeader.appendChild(p);
+
+
